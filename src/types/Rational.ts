d53b4aa68f7c6e6295e04902d3b597271f26a2be
Jason Barr <jason@jasonsbarr.com>
2023-05-03 21:04:09 -0500

    We do need an as cast, my bad

--- src/types/Rational.ts
+++ src/types/Rational.ts
@@ -8,5 +8,50 @@ export class Rational implements BoxedNumber {
     this.level = Levels.Rational;
   }
 
-  public toString(): string {}
+  // Comparison
+  greaterThan(other: BoxedNumber): boolean {}
+  greaterThanOrEqual(other: BoxedNumber): boolean {}
+  lessThan(other: BoxedNumber): boolean {}
+  lessThanOrEqual(other: BoxedNumber): boolean {}
+  equals(other: BoxedNumber): boolean {}
+
+  // Operations
+  add(other: BoxedNumber): BoxedNumber {}
+  subtract(other: BoxedNumber): BoxedNumber {}
+  multiply(other: BoxedNumber): BoxedNumber {}
+  divide(other: BoxedNumber): BoxedNumber {}
+  expt(other: BoxedNumber): BoxedNumber {}
+  numerator(): BoxedNumber {}
+  denominator(): BoxedNumber {}
+  integerSqrt(): BoxedNumber {}
+  sqrt(): BoxedNumber {}
+  floor(): BoxedNumber {}
+  ceiling(): BoxedNumber {}
+  conjugate(): BoxedNumber {}
+  magnitude(): BoxedNumber {}
+  log(): BoxedNumber {}
+  angle(): BoxedNumber {}
+  atan(): BoxedNumber {}
+  cos(): BoxedNumber {}
+  sin(): BoxedNumber {}
+  exp(): BoxedNumber {}
+  acos(): BoxedNumber {}
+  asin(): BoxedNumber {}
+  imaginaryPart(): BoxedNumber {}
+  realPart(): BoxedNumber {}
+  round(): BoxedNumber {}
+
+  // Predicates
+  isFinite(): boolean {}
+  isInteger(): boolean {}
+  isRational(): boolean {}
+  isReal(): boolean {}
+  isExact(): boolean {}
+
+  // Conversion
+  liftTo(n: BoxedNumber): BoxedNumber {}
+  toExact(): BoxedNumber {}
+  toJSNum(): number {}
+  toFixnum(): number {}
+  toString(): number {}
 }
