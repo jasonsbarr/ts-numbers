2602cfe53043aace0882568896aa328731f489d6
Jason Barr <jason@jasonsbarr.com>
2023-05-03 20:28:56 -0500

    Fill out makeFixnum function

--- src/utils/conversion.ts
+++ src/utils/conversion.ts
@@ -3,7 +3,15 @@ import { Number } from "src/types/Number";
 
 export function makeBignum(n: Number): Integer {}
 
-export function makeFixnum(n: Number): number {}
+export function makeFixnum(n: Number): number {
+  if (typeof n === "number") {
+    return n;
+  } else if (typeof n === "bigint") {
+    return Number(n);
+  }
+
+  return n.toFixnum();
+}
 
 export function numerator(n: Number) {
   if (typeof n === "number") {
